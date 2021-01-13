e86effaa70e57929378b8d81989524bbdb814d1c
dmr <rajan@bottle.com.np>
2021-01-13 19:43:39 +0545

    adds deleteUser function

---
+++ functions/users/deleteUser.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { getSuccessResponse } = require('../../helper/success');
+const { getErrorResponse } = require('../../helper/error');
+
+const { UsersModel } = require('../../Models/UserModel');
+
+module.exports.main = async (event) => {
+  try {
+    const request = JSON.parse(event.body);
+    const { id } = request;
+
+    const result = await UsersModel.delete({ id });
+    return getSuccessResponse(result);
+  } catch (error) {
+    return getErrorResponse(error);
+  }
+};
